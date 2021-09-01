6683d26628f64d1c1ef8a69e838a8ce0ca6eaf91
Yash.mathur <44976021+themockingjester@users.noreply.github.com>
2021-09-01 23:15:05 +0530

    dynamic programing

--- edit distance.py
+++ edit distance.py
@@ -19,3 +19,28 @@ class Solution:
                 self.recur(ans+b[currIndexSecond],currIndexFirst,currIndexSecond+1,a,b,ctr+1)                 #inserting
         else:
             return
+
+
+#                            Dynamic Programming (Bottom Up Approach)
+class Solution:
+	def editDistance(self, s, t):
+		# Code here
+		dp=[[0 for i in range(len(s)+1)] for j in range(len(t)+1)]
+		for i in range(len(t)+1):
+		    
+		    for j in range(len(s)+1):
+		        
+		        if i==0 and j==0:
+		            dp[i][j]=0
+		        elif j==0:
+		            dp[i][j]=1+dp[i-1][j]
+		        elif i==0:
+		            dp[i][j]=1+dp[i][j-1]
+		        else:
+		            if s[j-1]==t[i-1]:
+		                dp[i][j]=dp[i-1][j-1]
+		            else:
+		                dp[i][j]=min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])+1
+		return dp[-1][-1]
+		            
+		        
