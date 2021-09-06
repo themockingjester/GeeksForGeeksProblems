08102ab4ab6580c9283f9292edbc06cedffec3d0
Yash.mathur <44976021+themockingjester@users.noreply.github.com>
2021-09-06 14:40:10 +0530

    python3

---
+++ Immediate Smaller Element.py
@@ -0,0 +1,11 @@
+class Solution:
+
+	def immediateSmaller(self,arr,n):
+
+		for i in range(n-1):
+		    if arr[i]>arr[i+1]:
+		        arr[i]=arr[i+1]
+		    else:
+		        arr[i]=-1
+		arr[n-1]=-1
+		return arr
